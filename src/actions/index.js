9e34e5b66173dee4d0b96dc421e3417c1463eb1b
Kirill Matrosov <kdmatrosov@gmail.com>
2016-03-13 19:05:17 +0300

    add api call

---
+++ src/actions/index.js
@@ -0,0 +1,14 @@
+const API_KEY = '48f665c060e43a9ff7cc79f7268a22f5';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+import axios from 'axios';
+
+export const FETCH_WEATHER = 'FETCH_WEATHER'; // сделано, чтобы сохранить тип неизменным между действием и редьюсером. Сахар - сахар - сахар
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},ru`;
+  const request = axios.get(url);
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
