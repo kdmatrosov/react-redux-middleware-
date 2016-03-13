b34f479a1da4ed25269fde1a3f7eb562530d135e
Kirill Matrosov <kdmatrosov@gmail.com>
2016-03-13 17:53:51 +0300

    add state to container

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,12 +1,28 @@
 import React, {Component} from 'react';
 
 export default class SearchBar extends  Component{
+
+  constructor(props)
+  {
+    super(props);
+
+    this.state = {term: ''};
+    this.onInputChange = this.onInputChange.bind(this);
+  }
+  onInputChange(event)
+  {
+    console.log(event.target.value);
+    this.setState({term: event.target.value})
+  }
     render(){
         return (
             <form className="input-group">
-                <input type="text"/>
-                <span className="input-group-button">
-                    <button type="submit" className="btn  btn-secondary">Submit</button>
+                <input type="text" value={this.state.term}
+                onChange={this.onInputChange}
+                className='form-control'
+                placeholder="Прогноз погоды"/>
+                <span className="input-group-btn">
+                    <button type="submit" className="btn  btn-secondary">Искать</button>
                 </span>
             </form>
         );
